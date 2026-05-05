43a190dcdbecb53617df2f259f064bb2d9b5b1d2
Ethan Rouimi <contact@ethan-rouimi.com>
2026-05-05 11:58:41 +0200

    feat: fix compilation error led

--- ESP32/main/led.h
+++ ESP32/main/led.h
@@ -1,9 +1,7 @@
-#pragma once
-
-
 #ifndef _H_LED_
 #define _H_LED_ 1
 void blink_led(void);
 void configure_led(void);
-void set_led(int red,int green,int blue);
+void init_leds(void);
+void set_led(int red, int green, int blue);
 #endif
