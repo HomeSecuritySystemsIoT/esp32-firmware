a4891edb775c492df943ac6d8e77cb31ebf91970
Ethan Rouimi <contact@ethan-rouimi.com>
2026-05-06 16:39:46 +0200

    add parallel dns hijack

---
+++ ESP32/main/utilities/dns_hijack.c
@@ -0,0 +1,61 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lwip/sockets.h"
+
+static void dns_hijack_task(void *pvParameters) {
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	struct sockaddr_in server_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(53),
+		.sin_addr.s_addr = htonl(INADDR_ANY),
+	};
+	bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+	uint8_t buf[512];
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+
+	while (1) {
+		int len = recvfrom(sock, buf, sizeof(buf), 0,
+						   (struct sockaddr *)&client_addr, &client_len);
+		if (len < 12) continue;
+
+		// Build a DNS response pointing everything to 192.168.4.1
+		uint8_t resp[512];
+		memcpy(resp, buf, len);
+		resp[2] = 0x81;
+		resp[3] = 0x80; // flags: response, no error
+		resp[6] = 0x00;
+		resp[7] = 0x01; // 1 answer
+
+		int resp_len = len;
+		// Pointer back to the question name
+		resp[resp_len++] = 0xC0;
+		resp[resp_len++] = 0x0C;
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x01; // type A
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x01; // class IN
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x3C; // TTL 60s
+		resp[resp_len++] = 0x00;
+		resp[resp_len++] = 0x04; // rdlength 4
+		// 192.168.4.1
+		resp[resp_len++] = 192;
+		resp[resp_len++] = 168;
+		resp[resp_len++] = 4;
+		resp[resp_len++] = 1;
+
+		sendto(sock, resp, resp_len, 0, (struct sockaddr *)&client_addr,
+			   client_len);
+	}
+	close(sock);
+	vTaskDelete(NULL);
+}
+
+void start_dns_hijack(void) {
+	xTaskCreate(dns_hijack_task, "dns_hijack", 4096, NULL, 5, NULL);
+}
